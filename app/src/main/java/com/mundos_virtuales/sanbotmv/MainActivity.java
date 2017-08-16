c67568f42e4d329d31088c5c4234f3851a571c24
pdoren <pablo.saavedra@ug.uchile.cl>
2017-08-16 17:23:54 -0300

    Changed sdk version.

--- app/src/main/java/com/mundos_virtuales/sanbotmv/MainActivity.java
+++ app/src/main/java/com/mundos_virtuales/sanbotmv/MainActivity.java
@@ -10,11 +10,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.daimajia.slider.library.Animations.DescriptionAnimation;
-import com.daimajia.slider.library.SliderLayout;
-import com.daimajia.slider.library.SliderTypes.BaseSliderView;
-import com.daimajia.slider.library.SliderTypes.TextSliderView;
-import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.qihancloud.opensdk.base.TopBaseActivity;
 import com.qihancloud.opensdk.beans.FuncConstant;
 import com.qihancloud.opensdk.function.beans.FaceRecognizeBean;
@@ -34,14 +29,11 @@ import com.qihancloud.opensdk.function.unit.interfaces.speech.RecognizeListener;
 import com.qihancloud.opensdk.function.unit.interfaces.speech.SpeakListener;
 import com.qihancloud.opensdk.function.unit.interfaces.speech.WakenListener;
 
-import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends TopBaseActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
+public class MainActivity extends TopBaseActivity {
 
     String stringSpeechRecognition = "";
-
-    private SliderLayout mDemoSlider;
 
     Thread thRobot;
 
@@ -71,35 +63,6 @@ public class MainActivity extends TopBaseActivity implements BaseSliderView.OnSl
         mediaManager = (MediaManager) getUnitManager(FuncConstant.MEDIA_MANAGER);
 
         initListener();
-
-        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
-
-        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
-        file_maps.put("Slide1",R.drawable.slide1);
-        file_maps.put("Slide2",R.drawable.slide2);
-        file_maps.put("Slide3",R.drawable.slide3);
-
-        for(String name : file_maps.keySet()){
-            TextSliderView textSliderView = new TextSliderView(this);
-            // initialize a SliderLayout
-            textSliderView
-                    .description(name)
-                    .image(file_maps.get(name))
-                    .setScaleType(BaseSliderView.ScaleType.Fit)
-                    .setOnSliderClickListener(this);
-
-            //add your extra information
-            textSliderView.bundle(new Bundle());
-            textSliderView.getBundle()
-                    .putString("extra",name);
-
-            mDemoSlider.addSlider(textSliderView);
-        }
-        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
-        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
-        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
-        mDemoSlider.setDuration(4000);
-        mDemoSlider.addOnPageChangeListener(this);
 
         thRobot = new Thread(new TaskRobot());
     }
@@ -205,14 +168,7 @@ public class MainActivity extends TopBaseActivity implements BaseSliderView.OnSl
 
     @Override
     protected void onStop() {
-        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
-        mDemoSlider.stopAutoCycle();
         super.onStop();
-    }
-
-    @Override
-    public void onSliderClick(BaseSliderView slider) {
-        Toast.makeText(this,slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -225,10 +181,10 @@ public class MainActivity extends TopBaseActivity implements BaseSliderView.OnSl
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.action_video_test:
+            /*case R.id.action_video_test:
                 Intent intentVideoTest = new Intent(this, VideoTestActivity.class);
                 startActivity(intentVideoTest);
-                break;
+                break;*/
             case R.id.action_loop:
                 if(thRobot.isInterrupted()) {
                     thRobot.start();
@@ -248,15 +204,4 @@ public class MainActivity extends TopBaseActivity implements BaseSliderView.OnSl
         }
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-
-    @Override
-    public void onPageSelected(int position) {
-        Log.d("Slider Demo", "Page Changed: " + position);
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {}
 }
